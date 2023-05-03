fc28fb00cc4a9a313f0a6effc85ec589cda7f41f
Patlund <patrik.ojerssonlundgren@gmail.com>
2023-05-03 20:33:14 +0200

    Reset back to only AI created only.

--- examAI/src/AddNewItem.svelte
+++ examAI/src/AddNewItem.svelte
@@ -1,40 +1,46 @@
 <script>
-    import { onMount } from 'svelte';
-    // @ts-ignore
-    import Button from './Button.svelte';
-    
-    let name = '';
-    let country = '';
-    let hobby = '';
-    
-    const createItem = async () => {
-      const response = await fetch('/items', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name,
-          country,
-          hobby
-        })
-      });
-  
-      if (response.ok) {
-        // success, do something here
-        name = '';
-        country = '';
-        hobby = '';
-      } else {
-        // error handling here
-      }
-    };
-  
-    onMount(() => {
-      // highlight 'Add New Items' in the menu
+  import { onMount } from "svelte";
+  // @ts-ignore
+  import Button from "./Button.svelte";
+
+  let name = "";
+  let country = "";
+  let hobby = "";
+
+  const createItem = async () => {
+    const response = await fetch("/api/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        country,
+        hobby,
+      }),
     });
-  </script>
-  
+
+    if (response.ok) {
+      // success, do something here
+      name = "";
+      country = "";
+      hobby = "";
+    } else {
+      // error handling here
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createItem();
+  };
+
+  onMount(() => {
+    // highlight 'Add New Items' in the menu
+  });
+</script>
+
+<form on:submit={handleSubmit}>
   <div class="add-new-item">
     <div class="input-field">
       <label for="name">Enter your full name</label>
@@ -48,36 +54,37 @@
       <label for="hobby">What's your favourite hobby?</label>
       <input type="text" id="hobby" bind:value={hobby} />
     </div>
-    <Button color="green" on:click={createItem}>Create Item</Button>
+    <Button color="green" type="submit">Create Item</Button>
   </div>
-  
-  <style>
-    .add-new-item {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding: 2rem;
-      border: 1px solid #ccc;
-      border-radius: 0.25rem;
-      box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
-    }
-  
-    .input-field {
-      display: flex;
-      flex-direction: column;
-      margin-bottom: 1rem;
-      width: 100%;
-    }
-  
-    label {
-      font-weight: bold;
-      margin-bottom: 0.25rem;
-    }
-  
-    input {
-      padding: 0.5rem;
-      border: 1px solid #ccc;
-      border-radius: 0.25rem;
-      width: 100%;
-    }
-  </style>
+</form>
+
+<style>
+  .add-new-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+  }
+
+  .input-field {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 1rem;
+    width: 100%;
+  }
+
+  label {
+    font-weight: bold;
+    margin-bottom: 0.25rem;
+  }
+
+  input {
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    width: 100%;
+  }
+</style>
