a6769ada65f49f5e344004e51f8dbad95e542f96
Patlund <patrik.ojerssonlundgren@gmail.com>
2023-05-03 21:28:35 +0200

    Changes to make the CRUD functions work.

--- examAI/src/Modal.svelte
+++ examAI/src/Modal.svelte
@@ -1,52 +1,52 @@
 <script>
-    // @ts-ignore
-    import Button from './Button.svelte';
-    export let item;
-  
-    let name = item.name;
-    let country = item.country;
-    let hobby = item.hobby;
-  
-    function handleSubmit() {
-      const updatedItem = {
-        itemId: item.itemId,
-        name,
-        country,
-        hobby
-      };
-      fetch('/api/items', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(updatedItem)
+  // @ts-ignore
+  import Button from "./Button.svelte";
+  export let item;
+
+  let name = item.name;
+  let country = item.country;
+  let hobby = item.hobby;
+
+  function handleSubmit() {
+    const updatedItem = {
+      itemId: item.itemId,
+      name,
+      country,
+      hobby,
+    };
+    fetch(`/api/items/${item.itemId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedItem),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle successful update
       })
-        .then(response => response.json())
-        .then(data => {
-          // Handle successful update
-        })
-        .catch(error => {
-          console.error(error);
-          // Handle error
-        });
-    }
-  </script>
-  
-  <div class="modal">
-    <h2>Update Item</h2>
-    <div class="form-group">
-      <label for="name">Enter your full name</label>
-      <input type="text" id="name" bind:value={name}>
-    </div>
-    <div class="form-group">
-      <label for="country">Which country do you live in?</label>
-      <input type="text" id="country" bind:value={country}>
-    </div>
-    <div class="form-group">
-      <label for="hobby">What's your favourite hobby?</label>
-      <input type="text" id="hobby" bind:value={hobby}>
-    </div>
-    <div class="button-group">
-      <Button color="green" on:click={handleSubmit}>Update</Button>
-    </div>
+      .catch((error) => {
+        console.error(error);
+        // Handle error
+      });
+  }
+</script>
+
+<div class="modal">
+  <h2>Update Item</h2>
+  <div class="form-group">
+    <label for="name">Enter your full name</label>
+    <input type="text" id="name" bind:value={name} />
   </div>
+  <div class="form-group">
+    <label for="country">Which country do you live in?</label>
+    <input type="text" id="country" bind:value={country} />
+  </div>
+  <div class="form-group">
+    <label for="hobby">What's your favourite hobby?</label>
+    <input type="text" id="hobby" bind:value={hobby} />
+  </div>
+  <div class="button-group">
+    <Button color="green" on:click={handleSubmit}>Update</Button>
+  </div>
+</div>
