a8f29c68df898e7b825748244bb498391a5a42bc
Patlund <patrik.ojerssonlundgren@gmail.com>
2023-05-15 11:47:13 +0200

    Added all the code from ChatGPT4

--- examai2/src/App.svelte
+++ examai2/src/App.svelte
@@ -1,47 +1,47 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { activeMenu } from "./store";
+  import List from "./List.svelte";
+  import Create from "./Create.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<div class="menu">
+  <span
+    class="menu-item"
+    class:active={$activeMenu === "Items"}
+    on:click={() => $activeMenu.set("Items")}
+  >
+    Items
+  </span>
+  <span
+    class="menu-item"
+    class:active={$activeMenu === "Add New Items"}
+    on:click={() => $activeMenu.set("Add New Items")}
+  >
+    Add New Items
+  </span>
+</div>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+{#if $activeMenu === "Items"}
+  <List />
+{:else if $activeMenu === "Add New Items"}
+  <Create />
+{/if}
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .menu {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 50px;
+    background-color: #f0f0f0;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  .menu-item {
+    cursor: pointer;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  .menu-item.active {
+    color: red;
+    border-bottom: 2px solid red;
   }
 </style>
