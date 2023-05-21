c0b251d2d1792bcc87b154ead70b624f74d39f46
Patlund <patrik.ojerssonlundgren@gmail.com>
2023-05-21 10:59:36 +0200

    Missed a statement for ChatGPT-4.

--- examai2/src/List.svelte
+++ examai2/src/List.svelte
@@ -1,5 +1,6 @@
 <script>
     import { activeMenu } from "./store";
+    import { onMount } from "svelte";
     import Button from "./Button.svelte";
     import Modal from "./Modal.svelte";
 
@@ -37,11 +38,46 @@
 
 <!-- List component content and styles -->
 
-{#each items as item}
-    <!-- Render item details -->
-    <p>{item.name} - {item.country} - {item.hobby}</p>
-    <Button color="green" on:click={() => openModal(item)}>Edit</Button>
-    <Button color="red" on:click={() => deleteItem(item.itemId)}>Delete</Button>
-{/each}
+<div class="items-grid">
+    {#each items as item (item.itemId)}
+        <div class="item-box">
+            <ul class="item-list">
+                <li>Name: {item.name}</li>
+                <li>Country: {item.country}</li>
+                <li>Hobby: {item.hobby}</li>
+            </ul>
+            <Button color="green" on:click={() => openModal(item)}>Edit</Button>
+            <Button color="red" on:click={() => deleteItem(item.itemId)}
+                >Delete</Button
+            >
+        </div>
+    {/each}
+</div>
 
 <Modal bind:open={modalOpen} bind:currentItem on:updateItem={updateItem} />
+
+<style>
+    .items-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        gap: 1rem;
+        padding: 1rem;
+    }
+
+    .item-box {
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        padding: 1rem;
+        background-color: #f9f9f9;
+    }
+
+    .item-list {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    .item-list li {
+        margin-bottom: 0.5rem;
+    }
+</style>
